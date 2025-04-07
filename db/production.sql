3d638058478f65b3b167c04fc485f535c6380e33
Diego Charles <diegocharles@me.com>
2025-04-07 18:33:42 -0300

    Create the user for production

--- db/production.sql
+++ db/production.sql
@@ -1,1 +1,4 @@
 CREATE DATABASE heart_rate_monitor_production;
+CREATE USER heart_rate_monitor WITH PASSWORD 'postgres';
+GRANT ALL PRIVILEGES ON DATABASE heart_rate_monitor_production TO heart_rate_monitor;
+ALTER DATABASE heart_rate_monitor_production OWNER TO heart_rate_monitor;
